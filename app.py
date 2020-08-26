f0dff12e61e0390944d2f856c294341f50231bb1
DevCesarHdez <dev.cesar.hdez@gmail.com>
2020-08-26 17:15:02 -0500

    layout

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hellow world'
+    return render_template('layout.html')
 
 
 if __name__ == '__main__':
