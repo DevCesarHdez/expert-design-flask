5e2ae453de60340755c724b8c81f643d692f65b8
DevCesarHdez <dev.cesar.hdez@gmail.com>
2020-08-27 02:03:24 -0500

    route contact

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ def home():
     title = "Expert Design"
     return render_template('index.html', data = title)
 
+@app.route('/contact')
+def contact():
+    return "contact"
 
 if __name__ == '__main__':
     app.run(debug=True)
